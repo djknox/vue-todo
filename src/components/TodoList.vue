8676f7798d4341fb22792b255cd089d696660064
DJ Knox <dhknox@gmail.com>
2019-09-09 13:54:44 -0400

    create TodoList, TodoItem, and CreateTodo components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo-item v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo-item>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import TodoItem from './TodoItem.vue';
+
+export default {
+  props: ['todos'],
+  components: {
+    TodoItem,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+  },
+};
+</script>
